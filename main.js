bde6bc51525bec044ba0b3e8b4b92fed8f0b0c14
Finnegan <finnegan@adobe.com>
2019-03-01 17:44:56 -0500

    generates a random horse succesfully

--- main.js
+++ main.js
@@ -2,48 +2,12 @@
 var { Text, ImageFill, storage, Color, Rectangle } = require("scenegraph");
 const fs = require("uxp").storage.localFileSystem;
 
-// User picks an image file
-// const storage = require("uxp").storage;
-// const fs = storage.localFileSystem;
-// let imageFile = await fs.getFileForOpening({ types: storage.fileTypes.images });
-// let imageFile = ({ types: Image});
-
-//Get the plugin storage folder
-
-// const fs = storage.localFileSystem;
-
-// Loop over the contents of that folder 
-// until you find the file you want
-
-// const folder = await fs.getFolder();
-// const myNovel = (await fs.getEntries()).find(entry => entry.name.includes('novel'));
-
-// const folder = await fs.getFolder();
-// const myImages = (await fs.getEntries()).find(entry => entry.name.includes('images'));
-
-// const entries = await aFolder.getEntries();
-// const allFiles = entries.filter(entry => entry.isFile);
-
-// make a const of that file for use in imageFill
-
-// Create ImageFill for this image
-// const ImageFill = require("scenegraph").ImageFill;
-// let fill = new ImageFill(imageFile);
-// let fill = new ImageFill(imageFile);
-
-// Set fill of first selected item
-// selection.items[0].fill = fill;
-
-
-
 async function makeUnicorn(selection) {
     console.log("Time to make some unicorns!");
 
     const pluginFolder = await fs.getPluginFolder();
-    console.log("pluginFolder = " + pluginFolder);
-    console.log("fs = " + fs);
-    // console.log("");
-    // const fs = storage.localFileSystem;
+    // console.log("pluginFolder = " + pluginFolder);
+    // console.log("fs = " + fs);
 
     // Did you find the plugin folder?
     if (pluginFolder != null) {
@@ -63,45 +27,41 @@ async function makeUnicorn(selection) {
     }
 
     const folderContents = await myImages.getEntries();
-    folderContents.forEach(entry => console.log("Entry name = " + entry.name));
+    // folderContents.forEach(entry => console.log("Entry name = " + entry.name));
     const horseArray = [];
     folderContents.forEach(entry => horseArray.push(entry));
-    console.log("HorseArray = " + horseArray);
+    // console.log("HorseArray = " + horseArray);
+    console.log("HorseArray.length = " + horseArray.length);
 
-    const randomHorse = horseArray[Math.floor(Math.random() * horseArray.length)];
+    let randomNumber = (Math.floor(Math.random() * horseArray.length));
+    console.log("Random number is = " + randomNumber);
+    let randomHorse = horseArray[randomNumber];
 
-    // const UnicornImage = (await myImages.getEntries()).find(entry => entry.name.includes('unicorn.jpg'));
 
-
-    // Did you find the unicorn image?
-    // if (UnicornImage != null) {
-    //     console.log("I found the unicorn file.")
-    // } else {
-    //     console.log("I have no idea and UnicornImage = " + UnicornImage);
-    // }
-
-    const ImageFill = require("scenegraph").ImageFill;
+    let ImageFill = require("scenegraph").ImageFill;
     let unicornFill = new ImageFill(randomHorse);
+
+
+    const newElement = new Rectangle();
+    newElement.width = 500;
+    newElement.height = 335;
+    newElement.fill = unicornFill;
+    newElement.name = "🦄 It's a freaking unicorn 🦄";
+
+    selection.insertionParent.addChild(newElement);
+    newElement.moveInParentCoordinates(250, 250);
+
 
     sanityCheck();
 
     function sanityCheck() {
         let emoji = new Text();
+        selection.insertionParent.addChild(emoji);
         emoji.text = "🦄 \r Sane";
         emoji.fontSize = 100;
         emoji.fill = new Color("#E55EF1");
         emoji.name = "🦄 Sanity check";
-
-        selection.insertionParent.addChild(emoji);
         emoji.moveInParentCoordinates(0, 100);
-        const newElement = new Rectangle();
-        newElement.width = 500;
-        newElement.height = 335;
-        newElement.fill = unicornFill;
-        newElement.name = "🦄 It's a freaking unicorn 🦄";
-
-        selection.insertionParent.addChild(newElement);
-        newElement.moveInParentCoordinates(250, 250);
 
     }
 }
